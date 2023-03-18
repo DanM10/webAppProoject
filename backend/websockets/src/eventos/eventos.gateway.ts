f379dae4062d6690f60ddb9def9f8031e524f10b
DanielaRomero <daniela.romero@epn.edu.ec>
2023-03-18 17:00:36 -0500

    Carpets eventos

---
+++ backend/websockets/src/eventos/eventos.gateway.ts
@@ -0,0 +1,13 @@
+import {WebSocketGateway} from "@nestjs/websockets";
+
+@WebSocketGateway(
+    8086,
+    {
+        cors:{
+            origin: "*",
+        }
+    }
+)
+export class EventosGateway{
+    
+}
